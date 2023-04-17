f167fb88f9c4489d9c3fbbd35ecdabdf26014341
Samer Ismael <samerismaeelbat2@hotmail.com>
2023-04-17 21:32:27 +0200

    Adding try catch

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -6,26 +6,38 @@ public class MainMenu {
         DatabaseHandler db = new DatabaseHandler();
         OMBD omdb1 = new OMBD();
 
-        System.out.println("Do you have an API key? (y/n)");
-        Scanner scanner1 = new Scanner(System.in);
-        if (scanner1.nextLine().equalsIgnoreCase("y")) {
-            System.out.println("Enter your API key: ");
-            omdb1.setApiKey(scanner1.nextLine());
-        } else {
-            System.out.println("You can get an API key from http://www.omdbapi.com/");
-            System.out.println("Enter your API key: ");
-            omdb1.setApiKey(scanner1.nextLine());
+        try {
+            System.out.println("Do you have an API key? (y/n)");
+            Scanner scanner1 = new Scanner(System.in);
+            if (scanner1.nextLine().equalsIgnoreCase("y")) {
+                System.out.println("Enter your API key: ");
+                omdb1.setApiKey(scanner1.nextLine());
+            } else {
+                System.out.println("You can get an API key from http://www.omdbapi.com/");
+                System.out.println("Enter your API key: ");
+                omdb1.setApiKey(scanner1.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Wrong input!");
+            System.out.println(e.getMessage());
+            System.exit(0);
         }
 
-        System.out.println("Enter a movie title: ");
-        Scanner scanner = new Scanner(System.in);
-        String title = scanner.nextLine().toLowerCase();
+        try {
+            System.out.println("Enter a movie title: ");
+            Scanner scanner = new Scanner(System.in);
+            String title = scanner.nextLine().toLowerCase();
 
-        if (!db.getMovieFromDatabase(title)) {
-            System.out.println("Movie not found in database, searching online...");
-            omdb1.getData(omdb1.getApiKey(), title);
-            db.addMovieToDatabase(omdb1.getTitle().toLowerCase(), omdb1.getYear(), omdb1.getRated(), omdb1.getReleased());
-            omdb1.printMovie();
+            if (!db.getMovieFromDatabase(title)) {
+                System.out.println("Movie not found in database, searching online...");
+                omdb1.getData(omdb1.getApiKey(), title);
+                db.addMovieToDatabase(omdb1.getTitle().toLowerCase(), omdb1.getYear(), omdb1.getRated(), omdb1.getReleased());
+                omdb1.printMovie();
+            }
+        } catch (Exception e) {
+            System.out.println("Wrong input!");
+            System.out.println(e.getMessage());
+            System.exit(0);
         }
     }
 }
